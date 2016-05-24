a59c0a38fcfb238d1621f289ad90d643a11c1857
cly1231989 <349930507@qq.com>
2016-05-24 22:26:14 +0800

    Release Version

---
+++ QRCodeMaker/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace QRCodeMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logoTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.makebutton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.savePathTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.minNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.maxNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.minNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "生成内容";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(191, 42);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(519, 21);
+            this.textBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "LOGO图片";
+            // 
+            // logoTextBox
+            // 
+            this.logoTextBox.Location = new System.Drawing.Point(191, 89);
+            this.logoTextBox.Name = "logoTextBox";
+            this.logoTextBox.Size = new System.Drawing.Size(519, 21);
+            this.logoTextBox.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(728, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "生成序号";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(372, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "————";
+            // 
+            // makebutton
+            // 
+            this.makebutton.Location = new System.Drawing.Point(386, 259);
+            this.makebutton.Name = "makebutton";
+            this.makebutton.Size = new System.Drawing.Size(75, 23);
+            this.makebutton.TabIndex = 11;
+            this.makebutton.Text = "生成";
+            this.makebutton.UseVisualStyleBackColor = true;
+            this.makebutton.Click += new System.EventHandler(this.makebutton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(726, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "浏览";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // savePathTextBox
+            // 
+            this.savePathTextBox.Location = new System.Drawing.Point(191, 212);
+            this.savePathTextBox.Name = "savePathTextBox";
+            this.savePathTextBox.Size = new System.Drawing.Size(519, 21);
+            this.savePathTextBox.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "二维码保存路径";
+            // 
+            // minNumericUpDown
+            // 
+            this.minNumericUpDown.Location = new System.Drawing.Point(192, 147);
+            this.minNumericUpDown.Name = "minNumericUpDown";
+            this.minNumericUpDown.Size = new System.Drawing.Size(157, 21);
+            this.minNumericUpDown.TabIndex = 15;
+            // 
+            // maxNumericUpDown
+            // 
+            this.maxNumericUpDown.Location = new System.Drawing.Point(441, 146);
+            this.maxNumericUpDown.Name = "maxNumericUpDown";
+            this.maxNumericUpDown.Size = new System.Drawing.Size(157, 21);
+            this.maxNumericUpDown.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 606);
+            this.Controls.Add(this.maxNumericUpDown);
+            this.Controls.Add(this.minNumericUpDown);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.savePathTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.makebutton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.logoTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.minNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox logoTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button makebutton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox savePathTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown minNumericUpDown;
+        private System.Windows.Forms.NumericUpDown maxNumericUpDown;
+    }
+}
+
---
+++ QRCodeMaker/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ThoughtWorks;
+using ThoughtWorks.QRCode;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using System.IO;
+
+namespace QRCodeMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();//文件打开对话框 
+            openFileDialog.Filter = "JPEG|*.jpeg;*.jpg|位图文件|*.bmp|所有文件|*.*";//只能打开我们设置的这几类文件 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                logoTextBox.Text = fileName;
+                Bitmap newBmp = new Bitmap(fileName);
+                Bitmap bmp = new Bitmap(newBmp);
+                ORCodeImage = bmp;
+                logoImg = bmp;
+            }      
+        }
+
+        private object logoImg;//用于存放logo图片 
+        private Image ORCodeImage;
+  
+
+        /// <summary> 
+        /// 获取二维码生成图片        
+        /// </summary> 
+  
+        public Bitmap getcode(string writeStr)
+        {
+            QRCodeEncoder qRCodeEncoder = new QRCodeEncoder();
+            qRCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;//设置二维码编码格式 
+            qRCodeEncoder.QRCodeScale = 8;//设置编码测量度             
+            qRCodeEncoder.QRCodeVersion = 7;//设置编码版本   
+            qRCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;//设置错误校验 
+            Bitmap image = qRCodeEncoder.Encode(writeStr); 
+            return image;
+        }
+
+        /// <summary>         /// 获取logo         
+        /// </summary> 
+        /// <returns></returns>         
+        /// 
+        public Bitmap getlogo()         
+        { 
+            Bitmap newBmp = new Bitmap("logo.jpg");//获取图片对象             
+            Bitmap bmp = new Bitmap(newBmp, 30, 30);//缩放             
+            return bmp;         
+        }  
+
+        /// <summary>
+        /// /// 二维码保存         
+        /// </summary> 
+        /// <param name="sender"></param>         
+        /// <param name="e"></param> 
+        private void Save(string sFileName)         
+        { 
+            SaveFileDialog saveFile = new SaveFileDialog();//创建保存对话框 
+            saveFile.Filter = "JPEG|*.jpeg;*.jpg|位图文件|*.bmp|所有文件|*.*";//设置保存的图片格式  
+            if (ORCodeImage != null)
+            {
+                if (saveFile.ShowDialog() == DialogResult.OK)                 
+                { 
+                    string sFilePathName = saveFile.FileName;
+                    Image img = ORCodeImage;                    
+                    img.Save(sFilePathName);                 
+                }             
+            }             
+            else             
+            { 
+                MessageBox.Show("请先生成二维码！");             
+            }         
+        }  
+
+        private void makebutton_Click(object sender, EventArgs e)
+        {
+            string strText = textBox.Text; // "www.baidu.com/";
+
+            decimal minValue = minNumericUpDown.Value;
+            decimal maxValue = maxNumericUpDown.Value;
+            if (minValue > maxValue)
+            {
+                MessageBox.Show("序号范围非法，请重新输入");
+                return;
+            }
+
+            if (savePathTextBox.Text.Trim() == "")
+            {
+                MessageBox.Show("请设置保存位置");
+                return;
+            }
+
+            for (; minValue <= maxValue; minValue++ )
+            {
+                 if (strText.Trim() != "") //验证输入的生成内容是否为空             
+                 {
+                    Bitmap bCode = getcode(strText); //获取二维码图片                 
+                    if (logoImg == null)
+                    {
+                        ORCodeImage = bCode;
+                    }
+                    else
+                    {
+                        Bitmap bLogo = logoImg as Bitmap; //获取logo图片对象 
+                        bLogo = new Bitmap(bLogo, 80, 80); //改变图片的大小这里我们设置为30                     
+                        int Y = bCode.Height;
+                        int X = bCode.Width;
+                        Point point = new Point(X / 2 - 40, Y / 2 - 40);//logo图片绘制到二维码上，这里将简单计算一下logo所在的坐标 
+                        Graphics g = Graphics.FromImage(bCode);//创建一个画布                     
+                        g.DrawImage(bLogo, point);//将logo图片绘制到二维码图片上                     
+                        ORCodeImage = bCode;
+                    }
+
+                    string filePathName = savePathTextBox.Text + "\\" + minValue.ToString() + ".jpg";
+                    if (!Directory.Exists(savePathTextBox.Text))
+                        Directory.CreateDirectory(savePathTextBox.Text);
+                    ORCodeImage.Save(filePathName);
+                 }
+                 else
+                 {
+                     MessageBox.Show("输入生成内容！");
+                 }       
+            }
+
+            MessageBox.Show("二维码生成完成");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog saveFile = new FolderBrowserDialog();//创建保存对话框 
+            if (saveFile.ShowDialog() == DialogResult.OK)
+                savePathTextBox.Text = saveFile.SelectedPath;
+            //saveFile.Filter>      
+        }             
+    }
+}
